bcc1f791dc6dbaca805a4202d090df5418fc3d5a
nguyenvanca14062001-ops <nguyenvanca14062001@gmail.com>
2026-05-01 15:01:48 +0700

    fix: add base path to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Thêm dòng base này vào để sửa lỗi đường dẫn trên GitHub Pages
+  base: '/', 
   plugins: [
     vue(),
     vueDevTools(),
